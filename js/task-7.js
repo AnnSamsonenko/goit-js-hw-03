9192788e72626b97111b4aedaaa5fa0eedef70eb
Anna Samsonenko <foster.anna93@gmail.com>
2021-09-14 15:12:31 +0300

    add resoved task-7

--- js/task-7.js
+++ js/task-7.js
@@ -24,7 +24,13 @@ const account = {
    * Метод создает и возвращает объект транзакции.
    * Принимает сумму и тип транзакции.
    */
-  createTransaction(amount, type) {},
+  createTransaction(amount, type) {
+    this.transactions.push({
+      id: this.transactions.length + 1,
+      amount,
+      type,
+    });
+  },
 
   /*
    * Метод отвечающий за добавление суммы к балансу.
@@ -32,7 +38,10 @@ const account = {
    * Вызывает createTransaction для создания объекта транзакции
    * после чего добавляет его в историю транзакций
    */
-  deposit(amount) {},
+  deposit(amount) {
+    this.balance += amount;
+    this.createTransaction(amount, Transaction.DEPOSIT);
+  },
 
   /*
    * Метод отвечающий за снятие суммы с баланса.
@@ -43,21 +52,68 @@ const account = {
    * Если amount больше чем текущий баланс, выводи сообщение
    * о том, что снятие такой суммы не возможно, недостаточно средств.
    */
-  withdraw(amount) {},
+  withdraw(amount) {
+    this.balance -= amount;
+    this.createTransaction(amount, Transaction.WITHDRAW);
+  },
 
   /*
    * Метод возвращает текущий баланс
    */
-  getBalance() {},
+  getBalance() {
+    return this.balance;
+  },
 
   /*
    * Метод ищет и возвращает объект транзации по id
    */
-  getTransactionDetails(id) {},
+  getTransactionDetails(id) {
+    for (let transaction of this.transactions) {
+      if (transaction.id === id) {
+        return transaction;
+      }
+    }
+    return "Transaction doesn't exist";
+  },
 
   /*
    * Метод возвращает количество средств
    * определенного типа транзакции из всей истории транзакций
    */
-  getTransactionTotal(type) {},
+  getTransactionTotal(type) {
+    let totalSummary = 0;
+    for (let transaction of this.transactions) {
+      if (transaction.type === type) {
+        totalSummary += transaction.amount;
+      }
+    }
+    return totalSummary;
+  },
 };
+
+//ВЫЗОВЫ ФУНКЦИЙ
+
+console.log("Баланс: ", account.getBalance());
+account.deposit(50);
+console.log("Баланс: ", account.getBalance());
+account.deposit(150);
+console.log("Баланс: ", account.getBalance());
+account.deposit(100);
+console.log("Баланс: ", account.getBalance());
+account.withdraw(50);
+console.log("Баланс: ", account.getBalance());
+account.deposit(150);
+console.log("Баланс: ", account.getBalance());
+account.withdraw(50);
+console.log("Баланс: ", account.getBalance());
+console.log("История операций ", account.transactions);
+console.log("Информация о транзакции-1: ", account.getTransactionDetails(1));
+console.log("Информация о транзакции-7: ", account.getTransactionDetails(7));
+console.log(
+  "Сумма депозитов: ",
+  account.getTransactionTotal(Transaction.DEPOSIT)
+);
+console.log(
+  "Сумма cнятий: ",
+  account.getTransactionTotal(Transaction.WITHDRAW)
+);
