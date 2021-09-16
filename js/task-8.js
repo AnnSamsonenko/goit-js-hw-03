6e2a01c4fb90caa12b6e532cbdf8e028bb8f777a
Anna Samsonenko <foster.anna93@gmail.com>
2021-09-16 23:04:13 +0300

    add solved cart-task

--- js/task-8.js
+++ js/task-8.js
@@ -1,34 +1,3 @@
-// const books = [
-//   {
-//     title: "The Last Kingdom",
-//     author: "Bernard Cornwell",
-//     rating: 5,
-//   },
-//   {
-//     title: "На берегу спокойных вод",
-//     author: "Роберт Шекли",
-//     rating: 10,
-//   },
-// ];
-
-// for (const book of books) {
-//   let { title, author, rating } = book;
-//   if (title === "На берегу спокойных вод") {
-//     rating = 7;
-//     console.log(title);
-//     console.log(author);
-//     console.log("Рейтинг книги в цикле: ", rating); // 7
-//   }
-// }
-
-// console.log("рейтинг второй книги после цикла: ", books[1].rating); // 10
-
-// books[1].rating = 7;
-// console.log(
-//   "рейтинг второй книги после обращения через индекс: ",
-//   books[1].rating
-// ); // 7
-
 /*
  * Работем с коллекцией товаров в корзине:
  * - getItems()
@@ -50,12 +19,54 @@ const cart = {
   getItems() {
     return this.items;
   },
-  add(product) {},
-  remove(productName) {},
-  clear() {},
-  countTotalPrice() {},
-  increaseQuantity(productName) {},
-  decreaseQuantity(productName) {},
+
+  add(product) {
+    for (const item of this.items) {
+      if (item.name === product.name) {
+        item.quantity += 1;
+        return;
+      }
+    }
+
+    const newProduct = {
+      ...product,
+      quantity: 1,
+    };
+
+    this.items.push(newProduct);
+  },
+
+  remove(productName) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      if (productName === this.items[i].name) {
+        this.items.splice(i, 1);
+      }
+    }
+  },
+  clear() {
+    this.items = [];
+  },
+  countTotalPrice() {
+    let total = 0;
+    for (const { quantity, price } of this.items) {
+      total += quantity * price;
+    }
+    return total;
+  },
+  increaseQuantity(productName) {
+    for (const item of this.items) {
+      if (item.name === productName) {
+        item.quantity += 1;
+      }
+    }
+  },
+  decreaseQuantity(productName) {
+    for (const item of this.items) {
+      if (item.name === productName) {
+        item.quantity -= 1;
+      }
+    }
+  },
 };
 
 console.log(cart.getItems());
@@ -63,26 +74,31 @@ console.log(cart.getItems());
 cart.add({ name: "🍎", price: 50 });
 cart.add({ name: "🍇", price: 60 });
 cart.add({ name: "🍋", price: 60 });
+cart.add({ name: "🍓", price: 110 });
+cart.add({ name: "🍇", price: 60 });
+cart.add({ name: "🍋", price: 60 });
 cart.add({ name: "🍋", price: 60 });
 cart.add({ name: "🍓", price: 110 });
-cart.add({ name: "🍓", price: 110 });
+cart.add({ name: "🍎", price: 50 });
+cart.add({ name: "🍎", price: 50 });
+cart.add({ name: "🍎", price: 50 });
 cart.add({ name: "🍓", price: 110 });
 
-// console.table(cart.getItems());
+console.table(cart.getItems());
 
 // console.log('Total: ', cart.countTotalPrice());
 
-// cart.remove('🍇');
+// cart.remove("🍇");
 // console.table(cart.getItems());
 
 // cart.clear();
 // console.log(cart.getItems());
 
-// console.log('Total: ', cart.countTotalPrice());
+console.log("Total: ", cart.countTotalPrice());
 
-// cart.increaseQuantity('🍎');
+// cart.increaseQuantity("🍎");
 // console.table(cart.getItems());
 
-// cart.decreaseQuantity('🍋');
-// cart.decreaseQuantity('🍋');
+// cart.decreaseQuantity("🍋");
+// cart.decreaseQuantity("🍋");
 // console.table(cart.getItems());
